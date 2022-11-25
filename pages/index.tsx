00327cf97096e7a14bc9c4b672cccffdeaab3748
testFooBar71 <99458990+testFooBar71@users.noreply.github.com>
2022-11-24 22:42:50 -0400

    siu

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to el bicho siiuuu
         </h1>
 
         <p className={styles.description}>
